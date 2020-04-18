ff683b1cd75a7dab526a70fe17686c3cbf5a329c
ByteAlex <bytealex@zerotwo.bot>
2020-04-18 16:42:17 +0200

    Added scheduler settings to rabbitmq settings builder

--- rabbitmq/src/main/java/discord4j/connect/rabbitmq/ConnectRabbitMQSettings.java
+++ rabbitmq/src/main/java/discord4j/connect/rabbitmq/ConnectRabbitMQSettings.java
@@ -1,6 +1,8 @@
 package discord4j.connect.rabbitmq;
 
 import com.rabbitmq.client.Address;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.rabbitmq.ReceiverOptions;
 import reactor.rabbitmq.SenderOptions;
 
@@ -8,6 +10,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * Creates a new RabbitMQ settings object
+     *
      * @return settings object
      */
     public static ConnectRabbitMQSettings create() {
@@ -19,14 +22,18 @@ public class ConnectRabbitMQSettings {
 
     /**
      * Private constructor, use {@link ConnectRabbitMQSettings#create()} instead
+     * Defaults the schedulers to use {@link Schedulers#boundedElastic()}
      */
     private ConnectRabbitMQSettings() {
         this.senderOptions = new SenderOptions();
         this.receiverOptions = new ReceiverOptions();
+        withSubscriptionScheduler(Schedulers.boundedElastic());
+        withResourceManagementScheduler(Schedulers.boundedElastic());
     }
 
     /**
      * Set the RabbitMQs host-and-port list as {@link Address} array
+     *
      * @param addresses array of {@link Address} to which RabbitMQ nodes the client should connect
      * @return this builder
      */
@@ -38,6 +45,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * Sets the RabbitMQs host list with default ports
+     *
      * @param hosts array of hostnames to which RabbitMQ nodes the client should connect on default port
      * @return this builder
      */
@@ -51,6 +59,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * Sets the RabbitMQs host and port
+     *
      * @param host hostname to connect to
      * @param port port to connect to
      * @return this builder
@@ -62,6 +71,7 @@ public class ConnectRabbitMQSettings {
     /**
      * Sets the RabbitMQs host
      * This method will use the default port
+     *
      * @param host hostname to connect to
      * @return this builder
      */
@@ -71,6 +81,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * The username to use for authorization
+     *
      * @param user username for authorization
      * @return this builder
      */
@@ -82,6 +93,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * The password to use for authorization
+     *
      * @param password password for authorization
      * @return this builder
      */
@@ -92,7 +104,35 @@ public class ConnectRabbitMQSettings {
     }
 
     /**
+     * Uses a specific scheduler for connection subscriptions
+     *
+     * Set the scheduler to {@code null} to create one scheduler per instance
+     *
+     * @param scheduler the scheduler to use
+     * @return this builder
+     */
+    public ConnectRabbitMQSettings withSubscriptionScheduler(final Scheduler scheduler) {
+        senderOptions.connectionSubscriptionScheduler(scheduler);
+        receiverOptions.connectionSubscriptionScheduler(scheduler);
+        return this;
+    }
+
+    /**
+     * Uses a specific scheduler for resource management
+     *
+     * Set the scheduler to {@code null} to create one scheduler per instance
+     *
+     * @param scheduler the scheduler to use
+     * @return this builder
+     */
+    public ConnectRabbitMQSettings withResourceManagementScheduler(final Scheduler scheduler) {
+        senderOptions.resourceManagementScheduler(scheduler);
+        return this;
+    }
+
+    /**
      * Internal method to retrieve the whole settings object for the RabbitMQ sender
+     *
      * @return SenderOptions
      */
     SenderOptions getSenderOptions() {
@@ -101,6 +141,7 @@ public class ConnectRabbitMQSettings {
 
     /**
      * Internal method to retrieve the whole settings object for the RabbitMQ receiver
+     *
      * @return ReceiverOptions
      */
     ReceiverOptions getReceiverOptions() {
